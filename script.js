c997d3314ffc2ef7c27eb74f83278b8f2c3de47f
Tom Hackshaw <tom@tomhackshaw.com>
2023-04-11 12:08:35 +1200

    GeoJSON

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const map = L.map('map').setView([-36.8485, 174.7633], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken: 'pk.eyJ1IjoidG9tdXBvbSIsImEiOiJjbGdiaG94cmYwM25oM2VsaTZhZW9haGg1In0.9KpWucMh_XztKD--rD7Tbw'
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-fetch('galleries.json')
+fetch('galleries.geojson')
   .then((response) => response.json())
-  .then((galleries) => {
-    galleries.forEach((gallery) => {
-      const marker = L.marker([gallery.lat, gallery.lng]).addTo(map);
-      marker.bindPopup(`<b>${gallery.name}</b><br>${gallery.description}`);
-    });
+  .then((geojsonData) => {
+    L.geoJSON(geojsonData, {
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup(`<b>${feature.properties.name}</b><br>${feature.properties.description}`);
+      },
+    }).addTo(map);
   });

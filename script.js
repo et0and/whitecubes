3e24c694ab73be55f4a848610f86aef0b5ac37b4
Tom Hackshaw <tom@tomhackshaw.com>
2023-04-11 12:00:23 +1200

    Fixed map?

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const map = L.map('map').setView([-36.8485, 174.7633], 13);
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   id: 'mapbox/streets-v11',

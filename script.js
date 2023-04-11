4a9d7caf7f4a9ef6c68f676d731e26c522a8e2c8
Tom Hackshaw <tom@tomhackshaw.com>
2023-04-11 12:17:56 +1200

    Mobile support

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const map = L.map('map').setView([-36.8485, 174.7633], 13);
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+const defaultZoom = isMobile ? 12 : 13;
+const map = L.map('map').setView([-36.8485, 174.7633], defaultZoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',

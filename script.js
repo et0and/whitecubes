1a5e76d0d03d282360465439ca2e538792e4ec0f
Tom Hackshaw <tom@tomhackshaw.com>
2023-04-11 12:42:36 +1200

    Custom color markers added

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function createCustomIcon(color) {
+    return L.icon({
+      iconUrl: `https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+    });
+  }  
+
 const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 const defaultZoom = isMobile ? 12 : 13;
 const map = L.map('map').setView([-36.8485, 174.7633], defaultZoom);
@@ -6,16 +17,16 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-fetch('whitecubes.geojson')
+fetch('whitecubes.json')
   .then((response) => response.json())
-  .then((geojsonData) => {
-    L.geoJSON(geojsonData, {
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup(`<b>${feature.properties.name}</b><br>${feature.properties.description}`);
-      },
-    }).addTo(map);
+  .then((galleries) => {
+    galleries.forEach((gallery) => {
+      const customIcon = createCustomIcon(gallery.color);
+      const marker = L.marker([gallery.lat, gallery.lng], { icon: customIcon }).addTo(map);
+      marker.bindPopup(`<b>${gallery.name}</b><br>${gallery.description}`);
+    });
   });
-
+  
   class TitleControl extends L.Control {
     onAdd(map) {
       const title = L.DomUtil.create('div');

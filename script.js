4e3023c5eb824667a8d374cd212efd5f218049b8
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-03-26 22:41:32 +1300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const map = L.map('map', {
     }
 ).setView([-36.8485, 174.7633], defaultZoom);
   
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+const thunderforestTileUrl = `https://tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png?apikey=${process.env.THUNDERFOREST_API_KEY}`;
+
+L.tileLayer(thunderforestTileUrl, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 fetch('whitecubes.geojson')
